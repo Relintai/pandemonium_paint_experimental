c9e59d5b87c4d19ed32ed994a570ceeeb92ec838
Relintai <relintai@protonmail.com>
2022-11-15 18:26:43 +0100

    Initial test plugin code setup.

--- game/addons/paint/plugin.gd
+++ game/addons/paint/plugin.gd
@@ -1,10 +1,28 @@
 tool
 extends EditorPlugin
 
-
 func _enter_tree() -> void:
-	pass
+	var paint_editor_plugin : EditorPlugin = Engine.get_global("PaintEditorPlugin")
+	
+	if paint_editor_plugin:
+		var sidebar : PaintSidebar = paint_editor_plugin.get_sidebar()
+		
+		#print(sidebar)
 
 
 func _exit_tree() -> void:
-	pass
+	var paint_editor_plugin : EditorPlugin = Engine.get_global("PaintEditorPlugin")
+	
+	if paint_editor_plugin:
+		var sidebar : PaintSidebar = paint_editor_plugin.get_sidebar()
+		
+		#print(sidebar)
+
+
+#func _enter_tree() -> void:
+#	for c in get_parent().get_children():
+#		if c is EditorPlugin:
+#			var ep : EditorPlugin = c as EditorPlugin
+#
+#			if ep.get_class() == "PaintEditorPlugin":
+#				print(ep.get_class())

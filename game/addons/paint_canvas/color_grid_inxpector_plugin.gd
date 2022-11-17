34f92b4fb182040b79fbb94e5f6513f0bde796f5
Relintai <relintai@protonmail.com>
2022-11-17 17:47:13 +0100

    Use the new PaintProjectPropertyInspector.

--- game/addons/paint_canvas/color_grid_inxpector_plugin.gd
+++ game/addons/paint_canvas/color_grid_inxpector_plugin.gd
@@ -5,7 +5,7 @@ func can_handle(object: Object) -> bool:
 	return object is PaintNode
 	
 func parse_begin(object: Object) -> void:
-	var pc : PaintColorGrid = PaintColorGrid.new()
+	var pc : PaintProjectPropertyInspector = PaintProjectPropertyInspector.new()
 	pc.on_paint_node_selected(object)
 	add_custom_control(pc)
 	

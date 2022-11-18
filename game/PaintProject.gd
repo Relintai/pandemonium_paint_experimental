869bc95158b268accc55e77f85786db229e6c350
Relintai <relintai@protonmail.com>
2022-11-18 22:08:42 +0100

    Notes

--- game/PaintProject.gd
+++ game/PaintProject.gd
@@ -12,6 +12,31 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+# UNdo redo keybinds + undo redo tool buttons for PaintCanvas
+
+# add button that ads bg and visual grid, also should set owner so they get saved
+# canvas bg make it not use shader
+# inherit both from PaintNode -> automatic easy resize
+
+# Implement trickle down resize -> PAitnProject resized -> NOTIFICATION PAINT PROJECT RESIZED!
+
+# PaintNode -> current size property read only
+# Resize property + resize button
+
+# Mouse pos for the canvas property inspector
+
+# PaintProj -> Collect and merge and save -> NOTIF PaintProject PRE Save , then get image()
+# should be saved virtual! only true for canvas for now
+
+# PaintCanvas -> LoadImage
+
+# PaintNodes -> Outline property. Maybe keep node?
+
+# PaintCanvas -> save data in a compressed form
+
+# port back everything to c++
+
+# remove old deprecated classes
+
+
+

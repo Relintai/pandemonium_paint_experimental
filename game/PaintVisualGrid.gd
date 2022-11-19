df8e27f3327502738627bb157962bd0d374a6eab
Relintai <relintai@protonmail.com>
2022-11-19 02:32:18 +0100

    Updated the engine to get the new fixes and cleanups.

--- game/PaintVisualGrid.gd
+++ game/PaintVisualGrid.gd
@@ -1,9 +1,9 @@
 tool
 extends PaintVisualGrid
 
-export(int) var grid_size : int = 1 setget set_gs
-
-func set_gs(var n) -> void:
-	grid_size = n
-	set_grid_size(n)
-
+#export(int) var grid_size : int = 1 setget set_gs
+#
+#func set_gs(var n) -> void:
+#	grid_size = n
+#	set_grid_size(n)
+#

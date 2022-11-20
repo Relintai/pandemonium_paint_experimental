839caec1f2a26133452954f22bcf3449f8a8b45b
Relintai <relintai@protonmail.com>
2022-11-20 21:25:12 +0100

    Updated the engine.

--- game/PaintProject.gd
+++ game/PaintProject.gd
@@ -1,22 +1,7 @@
 tool
 extends PaintProject
 
-# Port the inspector plugin to c++ aswell
-
-# remove old deprecated classes (PaintCanvasOutline, PaintColorGrid too)
-# inherit both PaintVisualGrid and PaintCanvasBackground from PaintNode -> automatic easy resize
-
-# Rename the method that is used to set the inspected PaintNodes in PaintCustomPropertyInspector
-
-# New Tools:
-# PaintCanvas: Pixel Scale canvas, should be able to set interpolation (just need gui)
-
 # Make sure it works with touch
-
-# NO Mouse pos for the canvas property inspector ? The canvas editor should be able to do it with an overlay like in the 3d scene
-# NO PaintProject: Scale project ? (to px, or percent) (set scale for all sub nodes?) Might not work well, or just not like this
-# NO PaintProject Add open image and texture property, if image dragged to it it creates a canvas from it, if no children also adds vis grid, and bg + resizses itself
 
 # Eventually:
 # Filters, vectors ...
-
--- game/addons/paint_canvas/plugin.gd
+++ game/addons/paint_canvas/plugin.gd
@@ -13,10 +13,11 @@ func edit(object: Object) -> void:
 	active_canvas = object
 
 func _enter_tree() -> void:
-	ColorGridInspectorPlugin = load("res://addons/paint_canvas/color_grid_inxpector_plugin.gd")
+	pass
+	#ColorGridInspectorPlugin = load("res://addons/paint_canvas/color_grid_inxpector_plugin.gd")
 	
-	ip = ColorGridInspectorPlugin.new()
-	add_inspector_plugin(ip)
+	#ip = ColorGridInspectorPlugin.new()
+	#add_inspector_plugin(ip)
 	
 	#get_tree().connect("node_removed", self, "on_node_removed")
 	
@@ -28,8 +29,9 @@ func _enter_tree() -> void:
 		#print(sidebar)
 
 func _exit_tree() -> void:
-	if ip:
-		remove_inspector_plugin(ip)
+	pass
+	#if ip:
+	#	remove_inspector_plugin(ip)
 	
 #	var paint_editor_plugin : EditorPlugin = Engine.get_global("PaintEditorPlugin")
 #

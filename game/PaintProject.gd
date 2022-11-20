cb74b7d4f8b72255e0fe8bfcbb0a91d167d86b09
Relintai <relintai@protonmail.com>
2022-11-20 20:10:19 +0100

    Updated the engine.

--- game/PaintProject.gd
+++ game/PaintProject.gd
@@ -1,28 +1,21 @@
 tool
 extends PaintProject
 
-# the new tool should not take space when empty
 # Port the inspector plugin to c++ aswell
 
 # remove old deprecated classes (PaintCanvasOutline, PaintColorGrid too)
 # inherit both PaintVisualGrid and PaintCanvasBackground from PaintNode -> automatic easy resize
-# Rename the paint project editor which is present for all nodes to PaintNode editor
 
 # Rename the method that is used to set the inspected PaintNodes in PaintCustomPropertyInspector
 
-# PaintCanvas -> Only consume events if the current tool is select (probably should only route them in the plugin)
-
-# PaintProject Add open image and texture property, if image dragged to it it creates a canvas from it, if no children
-# also adds vis grid, and bg + resizses itself
-
 # New Tools:
-# PaintProject: Load image as new canvas, with proper gui
-# PaintProject: Scale project ? (to px, or percent) (set scale for all sub nodes?) Might not work well, or just not like this
 # PaintCanvas: Pixel Scale canvas, should be able to set interpolation (just need gui)
-# PaintCanvas -> LoadCanvasImage, exportCanvasImage -> TOOL
-# Mouse pos for the canvas property inspector
-# PaintProject: Move run properties to the inspector.
-# PaintProject: ExportImage -> dialog instead of text field # + run property. Can keel the field to save path though
+
+# Make sure it works with touch
+
+# NO Mouse pos for the canvas property inspector ? The canvas editor should be able to do it with an overlay like in the 3d scene
+# NO PaintProject: Scale project ? (to px, or percent) (set scale for all sub nodes?) Might not work well, or just not like this
+# NO PaintProject Add open image and texture property, if image dragged to it it creates a canvas from it, if no children also adds vis grid, and bg + resizses itself
 
 # Eventually:
 # Filters, vectors ...
